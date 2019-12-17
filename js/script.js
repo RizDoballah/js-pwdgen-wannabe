9205a348be552ce8efb6f948ddbc3ebfddabe03c
Ghezlene Doballah <ghyzounette@gmail.com>
2019-12-17 15:12:31 +0100

    final commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ alert('Hello World!');
+  var nome = prompt('quale il tuo nome?');
+  console.log(nome);
+  var cognome = prompt('quale il tuo cognome?');
+  console.log(cognome);
+  var colorePreferito = prompt('quale il tuo colore preferito?');
+  console.log(colorePreferito);
+  document.getElementById('password').innerHTML= nome+ cognome+colorePreferito+'19';
